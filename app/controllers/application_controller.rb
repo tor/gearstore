792178d40a62a7eb95870a3ee0adbf067a1a09f5
Tor Lattimore <tor.lattimore@gmail.com>
2012-07-10 17:18:41 +1000

    * fixed bug where heading to anumc.anu.edu.au/gs would crash if you weren't logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,13 @@ class ApplicationController < ActionController::Base
   protected
 
   def require_admin_login
-    @current_user = User.find(session[:user_id].to_i)
-    if not @current_user or not @current_user.admin?
+		if session[:user_id] 
+	    @current_user = User.find(session[:user_id].to_i)
+	    if not @current_user or not @current_user.admin?
+	      redirect_to logout_path
+	    end
+		else
       redirect_to logout_path
-    end
+		end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -76,6 +76,7 @@ Gs2::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'rentals#index'
+#  root :to => 'sessions#new', :as => :login
 
   # See how all your routes lay out with "rake routes"
 
