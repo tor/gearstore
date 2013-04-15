c7879919bfdd8f642c2e666918d2bec43a4800b4
Tor Lattimore <tor.lattimore@gmail.com>
2013-04-15 20:30:23 +1000

    * added sums of fees/deposits taken/deposits returned to ledger

--- app/models/ledger.rb
+++ app/models/ledger.rb
@@ -5,6 +5,18 @@ class Ledger < ActiveRecord::Base
 		Ledger.sum(:amount, :conditions => ['created_at <= ?', time])		
 	end
 
+  def self.fees_taken_today
+    return Ledger.sum(:amount, :conditions => ['created_at >= ? and description = ?', Time.now.beginning_of_day, 'fee'])
+  end
+
+  def self.deposits_taken_today
+    return Ledger.sum(:amount, :conditions => ['created_at >= ? and description = ?', Time.now.beginning_of_day, 'deposit'])
+  end
+
+  def self.deposits_returned_today
+    return Ledger.sum(:amount, :conditions => ['created_at >= ? and description like ?', Time.now.beginning_of_day, '%deposit returned%'])
+  end
+
 	def approver
 		User.find(approver_id)
 	end
