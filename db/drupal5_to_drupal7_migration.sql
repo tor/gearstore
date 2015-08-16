067045e55fcc8c15942f1a54c4ee2249a0c3ff7d
Pierre-Dominique Putallaz <pierredominique.putallaz@gmail.com>
2015-08-16 17:32:27 +1000

    Improve the migration script to add views and to wrap the DML statements into a transaction.

--- db/drupal5_to_drupal7_migration.sql
+++ db/drupal5_to_drupal7_migration.sql
@@ -1,3 +1,5 @@
+-- Uncomment to delete all the gs3 items in the drupal7 database.
+/*
 DROP TABLE drupal7.gs3_legacy_users;
 DROP TABLE drupal7.gs3_legacy_old_to_new;
 DROP TABLE drupal7.gs3_deposits;
@@ -9,6 +11,10 @@ DROP TABLE drupal7.gs3_rentals;
 DROP TABLE drupal7.gs3_user_notes;
 DROP TABLE drupal7.gs3_schema_migrations;
 DROP TABLE drupal7.gs3_ledgers;
+DROP VIEW drupal7.gs3_users;
+DROP VIEW drupal7.gs3_roles;
+DROP VIEW drupal7.gs3_roles_users;
+/* */
 
 -- Dump the content of the g3_users view into an actual table for later
 -- reconciliation.
@@ -33,6 +39,54 @@ INSERT INTO drupal7.gs3_schema_migrations SELECT * FROM anumc.gs3_schema_migrati
 CREATE TABLE drupal7.gs3_ledgers LIKE anumc.gs3_ledgers;
 INSERT INTO drupal7.gs3_ledgers SELECT * FROM anumc.gs3_ledgers;
 
+-- List of users from Drupal 7 database
+CREATE VIEW drupal7.gs3_users AS
+    SELECT 
+        drupal7.users.uid AS id,
+        drupal7.users.name AS username,
+        drupal7.users.pass AS pass,
+        drupal7.users.mail AS mail,
+        CONCAT_WS(_UTF8' ',
+                fn.field_account_first_name_value,
+                ln.field_account_last_name_value) AS name,
+        COALESCE(phone.field_account_home_phone_value,
+                workPhone.field_account_work_phone_value,
+                '') AS phone
+    FROM drupal7.users
+	JOIN drupal7.field_data_field_account_first_name fn 
+		ON fn.entity_id = drupal7.users.uid
+	JOIN drupal7.field_data_field_account_last_name ln 
+		ON ln.entity_id = drupal7.users.uid
+	LEFT JOIN drupal7.field_data_field_account_home_phone phone 
+		ON phone.entity_id = drupal7.users.uid
+	LEFT JOIN drupal7.field_data_field_account_work_phone workPhone 
+		ON workPhone.entity_id = drupal7.users.uid
+	-- Select all the users from the Drupal 5 dump.
+    UNION SELECT 
+        drupal7.gs3_legacy_users.id AS id,
+        drupal7.gs3_legacy_users.username AS username,
+        drupal7.gs3_legacy_users.pass AS pass,
+        drupal7.gs3_legacy_users.mail AS mail,
+        CONCAT(drupal7.gs3_legacy_users.name,
+                _UTF8' (old)') AS name,
+        drupal7.gs3_legacy_users.phone AS phone
+    FROM drupal7.gs3_legacy_users;
+        
+-- Roles and users.
+CREATE VIEW drupal7.gs3_roles_users AS
+    SELECT 
+		uid AS user_id, 
+        rid AS role_id
+    FROM drupal7.users_roles;
+
+CREATE VIEW drupal7.gs3_roles AS
+    SELECT 
+        rid AS id,
+        name
+    FROM drupal7.role;
+
+-- Re-key the users.
+START TRANSACTION;
 -- Update all the old keys to avoid clashes.
 UPDATE drupal7.gs3_legacy_users SET id = id + 500000;
 
@@ -100,5 +154,7 @@ JOIN drupal7.gs3_legacy_old_to_new o
 	ON o.old_id = t.approver_id
 SET t.approver_id = o.new_id;
 
+-- Commit all the changes if the script was successful.
+COMMIT;
 
 
