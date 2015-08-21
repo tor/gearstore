6eee6aa4c2a9dbc6c334f9ffc9c678b85294f44e
Pierre-Dominique Putallaz <pierredominique.putallaz@gmail.com>
2015-08-22 09:07:06 +1000

    Increment asset version

--- config/application.rb
+++ config/application.rb
@@ -54,6 +54,6 @@ module Gs2
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
   end
 end
